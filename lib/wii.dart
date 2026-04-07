46c5741f0842471e352ebdfad8c168a91cf13832
wtaype <wilder.taype16@gmail.com>
2026-04-06 23:27:47 -0500

    El mejor proyecto wiregistroo

--- lib/wii.dart
+++ lib/wii.dart
@@ -3,17 +3,17 @@ class wii {
   static const int lanzamiento = 2026;
   static const String autor = '@wilder.taype';
   static const String link = 'https://wtaype.github.io/';
-  static const String version = 'v10.1.1';
+  static const String version = 'v11.1.1';
 }
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v10 -m "Version v10" ; git push origin v10
+git tag v11 -m "Version v11" ; git push origin v11
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v10.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v11.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v10 ; git tag v10 -m "Version v10 actualizada" ; git push origin v10 --force
+git tag -d v11 ; git tag v11 -m "Version v11 actualizada" ; git push origin v11 --force
 
 flutter clean
 flutter pub get
